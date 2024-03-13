35714c12d4f2774dfae71f7baeb3ee484fcf4c5c
pocarisweat257 <98939867+pocarisweat257@users.noreply.github.com>
2024-03-13 21:54:39 +0900

    Update bsw.cpp

--- 05. ButtonISR/bsw.cpp
+++ 05. ButtonISR/bsw.cpp
@@ -1,13 +1,14 @@
 #include "ee.h"
 #include "Arduino.h"
 #include "bsw.h"
-// Write the code below.
+#include <avr/io.h>
+#include <avr/interrupt.h>
 
 #define TIMER1_US	1000000U	/* 1 sec */
 
 #define LEN_BUF 128
 
-// Write the code below.
+#define _BV(bit) (1 << (bit))
 
 extern "C"{
 
@@ -49,8 +50,11 @@ void setup(void)
 
 int main(void)
 {
-	// Write the code below.
-
+	PORTC = _BV(PC0);
+	PCICR = _BV(PCIE1);
+	PCMSK1 = _BV(PCINT8);
+	EIFR = 0xff;
+	sei();
 	init();
 
 #if defined(USBCON)
