4626e46b478e0784d484382e015d46aa4823e701
hamin1483 <anhm720@naver.com>
2025-03-05 14:07:20 +0900

    del2

--- cp4ad-rots+can/erika/obj/ee_avr8_asm.S.d
+++
@@ -1,2 +0,0 @@
-obj/ee_avr8_asm.S.o: \
- /cygdrive/c/Users/anhamin/Desktop/CP4AD-~4/erika/src/ee_avr8_asm.S
--- cp4ad-rtos-main-answer/01. Hello World/asw.c
+++
@@ -1,8 +0,0 @@
-#include "bsw.h"
-
-TASK(Task1)
-{
-	printfSerial("Hello World!\n");
-
-	TerminateTask();
-}
