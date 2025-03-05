12b51da10484c97b8f6a8b5704c088927af0bbbe
hamin1483 <anhm720@naver.com>
2025-03-05 13:55:05 +0900

    w/o build

---
+++ cp4ad-rots+can/erika/obj/ee_avr8_asm.S.d
@@ -0,0 +1,2 @@
+obj/ee_avr8_asm.S.o: \
+ /cygdrive/c/Users/anhamin/Desktop/CP4AD-~4/erika/src/ee_avr8_asm.S
---
+++ cp4ad-rots+can/out/obj/LiquidCrystal.cpp.d
@@ -0,0 +1,41 @@
+obj/LiquidCrystal.cpp.o: \
+ /cygdrive/c/Users/anhamin/Desktop/CP4AD-~4/LiquidCrystal.cpp \
+ /cygdrive/c/Users/anhamin/Desktop/CP4AD-~4/LiquidCrystal.h \
+ /cygdrive/c/Arduino/hardware/arduino/avr/cores/arduino/Print.h \
+ /cygdrive/c/Arduino/hardware/arduino/avr/cores/arduino/WString.h \
+ /cygdrive/c/Arduino/hardware/arduino/avr/cores/arduino/Printable.h \
+ /cygdrive/c/Arduino/hardware/arduino/avr/cores/arduino/Arduino.h \
+ /cygdrive/c/Arduino/hardware/arduino/avr/cores/arduino/binary.h \
+ /cygdrive/c/Arduino/hardware/arduino/avr/cores/arduino/WCharacter.h \
+ /cygdrive/c/Arduino/hardware/arduino/avr/cores/arduino/HardwareSerial.h \
+ /cygdrive/c/Arduino/hardware/arduino/avr/cores/arduino/Stream.h \
+ /cygdrive/c/Arduino/hardware/arduino/avr/cores/arduino/Print.h \
+ /cygdrive/c/Arduino/hardware/arduino/avr/cores/arduino/USBAPI.h \
+ /cygdrive/c/Arduino/hardware/arduino/avr/cores/arduino/Arduino.h \
+ /cygdrive/c/Arduino/hardware/arduino/avr/variants/standard/pins_arduino.h
+
+/cygdrive/c/Users/anhamin/Desktop/CP4AD-~4/LiquidCrystal.h:
+
+/cygdrive/c/Arduino/hardware/arduino/avr/cores/arduino/Print.h:
+
+/cygdrive/c/Arduino/hardware/arduino/avr/cores/arduino/WString.h:
+
+/cygdrive/c/Arduino/hardware/arduino/avr/cores/arduino/Printable.h:
+
+/cygdrive/c/Arduino/hardware/arduino/avr/cores/arduino/Arduino.h:
+
+/cygdrive/c/Arduino/hardware/arduino/avr/cores/arduino/binary.h:
+
+/cygdrive/c/Arduino/hardware/arduino/avr/cores/arduino/WCharacter.h:
+
+/cygdrive/c/Arduino/hardware/arduino/avr/cores/arduino/HardwareSerial.h:
+
+/cygdrive/c/Arduino/hardware/arduino/avr/cores/arduino/Stream.h:
+
+/cygdrive/c/Arduino/hardware/arduino/avr/cores/arduino/Print.h:
+
+/cygdrive/c/Arduino/hardware/arduino/avr/cores/arduino/USBAPI.h:
+
+/cygdrive/c/Arduino/hardware/arduino/avr/cores/arduino/Arduino.h:
+
+/cygdrive/c/Arduino/hardware/arduino/avr/variants/standard/pins_arduino.h:
